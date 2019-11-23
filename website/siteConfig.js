032894e56563fa0c90a856422fd2095c44a8df41
cenzhipeng <cenzhipeng@aliyun.com>
2019-11-23 18:24:44 +0800

    添加复制按钮

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -127,7 +127,7 @@ const siteConfig = {
     'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
     '/js/code-block-buttons.js',
   ],
-  stylesheets: ['/css/code-block-buttons.css']
+  // stylesheets: ['/css/code-block-buttons.css']
 };
 
 module.exports = siteConfig;
