35bfaca31b980d14e5b346cd75dd3c236135e846
cenzhipeng <cenzhipeng@aliyun.com>
2019-11-23 18:05:10 +0800

    添加复制按钮

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -121,7 +121,13 @@ const siteConfig = {
   scrollToTop: true,
   scrollToTopOptions: {
     scrollDuration: 400
-  }
+  },
+  scripts: [
+    'https://buttons.github.io/buttons.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
+    '/js/code-block-buttons.js',
+  ],
+  stylesheets: ['/css/code-block-buttons.css']
 };
 
 module.exports = siteConfig;
