bc919507c23009fbfe6af7a5e37045d2c6bcfa93
cenzhipeng <cenzhipeng@aliyun.com>
2019-11-24 15:34:34 +0800

    修改配置

---
+++ website/custom_config.sh
@@ -0,0 +1,7 @@
+echo -e 'User-agent: * \nAllow: /' > robots.txt
+for var in $(find .  -iname "*.html" |grep -v /en/); \
+do \
+sed -i 's/html lang=""/html lang="zh-CN"/' $var; \
+sed -i 's/html lang="en"/html lang="zh-CN"/' $var; \
+sed -i 's!https://unpkg.com/vanilla-back-to-top@7.1.14/dist/vanilla-back-to-top.min.js!https://cdn.jsdelivr.net/npm/vanilla-back-to-top@7.1.14/dist/vanilla-back-to-top.min.js!' $var; \
+done
