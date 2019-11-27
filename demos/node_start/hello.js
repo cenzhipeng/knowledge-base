7eb1cb6bee7f789be7dd175c48285e4f8a0423db
cenzhipeng <cen-zhipeng@qq.com>
2019-11-27 08:56:43 +0800

    修改路径

--- demos/node_start/hello.js
+++ demos/node_start/hello.js
@@ -1,17 +1,6 @@
-var sobj1 = new String('abc');
-var sobj2 = new String('abc');
-console.log(sobj1 == sobj2); // false 2者不是同一个 String 对象的引用
-console.log(sobj1 === sobj2); // false 理由同上
-console.log(sobj1 + '' == sobj2 + ''); // true 2者都是 string，且内容相同
-console.log(sobj1 + '' === sobj2 + ''); // true 2者都是 string，且内容相同
-var sobj = new String('abc'); 
-var s = 'abc';
-console.log(sobj == s); // true 2者转换后的值相等
-console.log(sobj === s); // false 2者类型不等
-
-var sobj = new String('abc');
-console.log(typeof sobj); // object
-console.log(typeof 'sobj') // string
-
-var abc = String(123);
-console.log(typeof abc) // string
+var http = require('http');
+http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello World\n');
+}).listen(8124);
+console.log('Server running at http://127.0.0.1:8124/');
